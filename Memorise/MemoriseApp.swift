77b503c7c5b733be58e677cd50e36eef33e28e96
iJackWilson <jack.wilson8796@gmail.com>
2021-02-14 14:37:34 +0000

    First commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Jack Wilson on 14/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
